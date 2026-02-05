35b4498077c4e8d14d8836535b0b9f4eea3eabaa
Wildan Al Irsyad Putra Wachyudi <wildan.al2362@sma.belajar.id>
2026-02-05 18:51:19 +0700

    Implement basic network driver structure
    Added basic structure for a network driver with init and exit functions.

--- drivers/network_driver.c
+++ drivers/network_driver.c
@@ -1,1 +1,20 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
 
+static int __init network_driver_init(void) {
+    printk(KERN_INFO "Loading Network Driver...\n");
+    // TODO: Initialize network driver
+    return 0;
+}
+
+static void __exit network_driver_exit(void) {
+    printk(KERN_INFO "Unloading Network Driver.\n");
+    // TODO: Cleanup network driver resources
+}
+
+module_init(network_driver_init);
+module_exit(network_driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("WildanDeveloper");
+MODULE_DESCRIPTION("Simple Network Driver Template");
